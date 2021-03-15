a3a4a6c408412f8084e24daaa215249adc71fb26
sorgelig <pour.garbage@gmail.com>
2021-03-15 18:57:20 +0800

    Update sys.

--- sys/video_freak.sv
+++ sys/video_freak.sv
@@ -1,6 +1,10 @@
 //
 //
-// Copyright (c) 2020 Grabulosaure
+// Video crop 
+// Copyright (c) 2020 Grabulosaure, (c) 2021 Alexey Melnikov
+// 
+// Integer scaling
+// Copyright (c) 2021 Alexey Melnikov
 //
 // This program is GPL Licensed. See COPYING for the full license.
 //
@@ -8,8 +12,6 @@
 ////////////////////////////////////////////////////////////////////////////////////////////////////////
 
 `timescale 1ns / 1ps
-
-// Modified version to display specific vertical size. // Sorgelig
 
 module video_freak
 (
@@ -25,10 +27,16 @@ module video_freak
 	input             VGA_DE_IN,
 	input      [11:0] ARX,
 	input      [11:0] ARY,
-	input       [9:0] CROP_SIZE,
-	input       [4:0] CROP_OFF,
-	input       [1:0] SCALE
+	input      [11:0] CROP_SIZE,
+	input       [4:0] CROP_OFF, // -16...+15
+	input       [2:0] SCALE     //0 - normal, 1 - V-integer, 2 - HV-Integer-, 3 - HV-Integer+, 4 - HV-Integer
 );
+
+reg         mul_start;
+wire        mul_run;
+reg  [11:0] mul_arg1, mul_arg2;
+wire [23:0] mul_res;
+sys_umul #(12,12) mul(CLK_VIDEO,mul_start,mul_run, mul_arg1,mul_arg2,mul_res);
 
 reg        vde;
 reg [11:0] arxo,aryo;
@@ -36,11 +44,13 @@ reg [11:0] vsize;
 reg [11:0] hsize;
 
 always @(posedge CLK_VIDEO) begin
-	reg        old_de, old_vs,vcalc;
-	reg  [9:0] vtot,vcpt,vcrop,voff;
+	reg        old_de, old_vs,ovde;
+	reg [11:0] vtot,vcpt,vcrop,voff;
 	reg [11:0] hcpt;
 	reg [11:0] vadj;
-	reg [21:0] ARXI,ARYI,ARXG,ARYG,arx,ary;
+	reg [23:0] ARXG,ARYG;
+	reg [11:0] arx,ary;
+	reg  [1:0] vcalc;
 
 	if (CE_PIXEL) begin
 		old_de <= VGA_DE_IN;
@@ -49,7 +59,7 @@ always @(posedge CLK_VIDEO) begin
 			vcpt  <= 0;
 			vtot  <= vcpt;
 			vcalc <= 1;
-			vcrop <= ((CROP_SIZE >= vcpt) || !CROP_SIZE) ? 10'd0 : CROP_SIZE;
+			vcrop <= (CROP_SIZE >= vcpt) ? 12'd0 : CROP_SIZE;
 		end
 		
 		if (VGA_DE_IN) hcpt <= hcpt + 1'd1;
@@ -63,33 +73,89 @@ always @(posedge CLK_VIDEO) begin
 	arx <= ARX;
 	ary <= ARY;
 
-	vsize <= vcrop;
+	vsize <= vcrop ? vcrop : vtot;
+	
+	mul_start <= 0;
 
 	if(!vcrop || !ary || !arx) begin
-		arxo  <= arx[11:0];
-		aryo  <= ary[11:0];
-		vsize <= vtot;
+		arxo  <= arx;
+		aryo  <= ary;
 	end
 	else if (vcalc) begin
-		ARXG <= arx * vtot;
-		ARYG <= ary * vcrop;
-		vcalc <= 0;
+		if(~mul_start & ~mul_run) begin
+			vcalc <= vcalc + 1'd1;
+			case(vcalc)
+				1: begin
+						mul_arg1  <= arx;
+						mul_arg2  <= vtot;
+						mul_start <= 1;
+					end
+
+				2: begin
+						ARXG      <= mul_res;
+						mul_arg1  <= ary;
+						mul_arg2  <= vcrop;
+						mul_start <= 1;
+					end
+
+				3: begin
+						ARYG      <= mul_res;
+					end
+			endcase
+		end
 	end
-	else if (ARXG[21] | ARYG[21]) begin
-		arxo <= ARXG[21:10];
-		aryo <= ARYG[21:10];
+	else if (ARXG[23] | ARYG[23]) begin
+		arxo <= ARXG[23:12];
+		aryo <= ARYG[23:12];
 	end
 	else begin
 		ARXG <= ARXG << 1;
 		ARYG <= ARYG << 1;
 	end
 
-	vadj <= {2'b0,vtot-vcrop} + {{6{CROP_OFF[4]}},CROP_OFF,1'b0};
-	voff <= vadj[11] ? 10'd0 : ((vadj[11:1] + vcrop) > vtot) ? vtot-vcrop : vadj[10:1];
-	vde  <= ((vcpt >= voff) && (vcpt < (vcrop + voff))) || !vcrop;
+	vadj <= (vtot-vcrop) + {{6{CROP_OFF[4]}},CROP_OFF,1'b0};
+	voff <= vadj[11] ? 12'd0 : ((vadj[11:1] + vcrop) > vtot) ? vtot-vcrop : vadj[11:1];
+	ovde <= ((vcpt >= voff) && (vcpt < (vcrop + voff))) || !vcrop;
+	vde  <= ovde;
 end
 
 assign VGA_DE = vde & VGA_DE_IN;
+
+video_scale_int scale
+(
+	.CLK_VIDEO(CLK_VIDEO),
+	.HDMI_WIDTH(HDMI_WIDTH),
+	.HDMI_HEIGHT(HDMI_HEIGHT),
+	.SCALE(SCALE),
+	.hsize(hsize),
+	.vsize(vsize),
+	.arx_i(arxo),
+	.ary_i(aryo),
+	.arx_o(VIDEO_ARX),
+	.ary_o(VIDEO_ARY)
+);
+
+endmodule
+
+
+module video_scale_int
+(
+	input             CLK_VIDEO,
+
+	input      [11:0] HDMI_WIDTH,
+	input      [11:0] HDMI_HEIGHT,
+
+	input       [2:0] SCALE,
+
+	input      [11:0] hsize,
+	input      [11:0] vsize,
+
+	input      [11:0] arx_i,
+	input      [11:0] ary_i,
+
+	output reg [12:0] arx_o,
+	output reg [12:0] ary_o
+);
 
 reg         div_start;
 wire        div_run;
@@ -107,15 +173,17 @@ sys_umul #(12,12) mul(CLK_VIDEO,mul_start,mul_run, mul_arg1,mul_arg2,mul_res);
 wire [11:0] wideres = mul_res[11:0] + hsize;
 
 always @(posedge CLK_VIDEO) begin
-	reg [11:0] oheight;
-	reg  [2:0] cnt;
+	reg [11:0] oheight,wres;
+	reg [12:0] arxf,aryf;
+	reg  [3:0] cnt;
+	reg        narrow;
 
 	div_start <= 0;
 	mul_start <= 0;
 
-	if (!SCALE || !aryo || !arxo) begin
-		VIDEO_ARX <= arxo;
-		VIDEO_ARY <= aryo;
+	if (!SCALE || (!ary_i && arx_i)) begin
+		arxf <= arx_i;
+		aryf <= ary_i;
 	end
 	else if(~div_start & ~div_run & ~mul_start & ~mul_run) begin
 		cnt <= cnt + 1'd1;
@@ -126,43 +194,85 @@ always @(posedge CLK_VIDEO) begin
 					div_start <= 1;
 				end
 
-			1: begin
+			1: if(!div_res[11:0]) begin
+					// screen resolution is lower than video resolution.
+					// Integer scaling is impossible.
+					arxf      <= arx_i;
+					aryf      <= ary_i;
+					cnt       <= 0;
+				end
+				else begin
 					mul_arg1  <= vsize;
-					mul_arg2  <= div_res[11:0];;
+					mul_arg2  <= div_res[11:0];
 					mul_start <= 1;
 				end
 
 			2: begin
 					oheight   <= mul_res[11:0];
+					if(!ary_i) begin
+						cnt    <= 8;
+					end
+				end
+				
+			3: begin
 					mul_arg1  <= mul_res[11:0];
-					mul_arg2  <= arxo;
+					mul_arg2  <= arx_i;
 					mul_start <= 1;
 				end
 
-			3: begin
-					div_num   <= mul_res;
-					div_den   <= aryo;
-					div_start <= 1;
-				end
-
 			4: begin
-					div_num   <= div_res[11:0];
-					div_den   <= hsize;
+					div_num   <= mul_res;
+					div_den   <= ary_i;
 					div_start <= 1;
 				end
 
 			5: begin
+					div_num   <= div_res;
+					div_den   <= hsize;
+					div_start <= 1;
+				end
+
+			6: begin
 					mul_arg1  <= hsize;
 					mul_arg2  <= div_res[11:0] ? div_res[11:0] : 12'd1;
 					mul_start <= 1;
 				end
 
-			6: begin
-					VIDEO_ARX <= {1'b1, ~SCALE[1] ? div_num[11:0] : (SCALE[0] && (wideres <= HDMI_WIDTH)) ? wideres : mul_res[11:0]};
-					VIDEO_ARY <= {1'b1, oheight};
+			7: if(mul_res <= HDMI_WIDTH) begin
+					cnt       <= 10;
+				end
+
+			8:	begin
+					div_num   <= HDMI_WIDTH;
+					div_den   <= hsize;
+					div_start <= 1;
+				end
+
+			9: begin
+					mul_arg1  <= hsize;
+					mul_arg2  <= div_res[11:0] ? div_res[11:0] : 12'd1;
+					mul_start <= 1;
+				end
+
+			10: begin
+					narrow    <= ((div_num[11:0] - mul_res[11:0]) <= (wideres - div_num[11:0])) || (wideres > HDMI_WIDTH);
+					wres      <= wideres;
+				end
+
+			11: begin
+					case(SCALE)
+							2: arxf <= {1'b1, mul_res[11:0]};
+							3: arxf <= {1'b1, (wres > HDMI_WIDTH) ? mul_res[11:0] : wres};
+							4: arxf <= {1'b1,              narrow ? mul_res[11:0] : wres};
+					default: arxf <= {1'b1, div_num[11:0]};
+					endcase
+					aryf <= {1'b1, oheight};
 				end
 		endcase
 	end
+
+	arx_o <= arxf;
+	ary_o <= aryf;
 end
 
 endmodule
